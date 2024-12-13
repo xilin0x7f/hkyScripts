81362e76b2e6b45c2194fc02c7f51bf031efbbdc
xilin0x7f <hkychn@outlook.com>
2024-12-13 14:27:26 +0800

    add volume fpca function

--- hky.py
+++ hky.py
@@ -53,6 +53,73 @@ def setup_volume_restore(subparsers):
     parser.add_argument("save_path", help="save path")
 
 @arg_extractor
+def fpca(data, atlas, out):
+    """
+    fpca 用于进行Functional pca分析，并保存均值、Vmat到本地以便后续应用, 以保存的文件为pca_mean.nii.gz,
+    roi1的Vmat为pca_Vmat_00001.nii.gz为例，用fsl, workbench提取all_FA_skeletonised_s2, roi1的主成分
+    fslmaths all_FA_skeletonised_s2 -sub pca_mean all_FA_skeletonised_s2_demeaned
+    fslroi pca_Vmat_00001 roi1_pc1_vmat 0 1
+    fslmaths all_FA_skeletonised_s2_demeaned -mul roi1_pc1_vmat roi1_pc1_res
+    wb_command -volume-stats roi1_pc1_res.nii.gz -reduce SUM
+    输出结果结果为roi1 pc1的主成分
+    """
+    data_file = nib.load(data)
+    atlas_file = nib.load(atlas)
+
+    if np.linalg.norm(data_file.affine - atlas_file.affine) > 1e-6:
+        raise ValueError("data and atlas do not have the same affine")
+
+    data = data_file.get_fdata()
+    atlas = atlas_file.get_fdata()
+
+    if not data.shape[:3] == atlas.shape:
+        raise ValueError("data and atlas do not have the same shape")
+
+    # time * space
+    dat2d = data.reshape(-1, data.shape[3]).T
+    dat2d_mean = np.mean(dat2d, axis=0)
+    nifti_image = nib.Nifti1Image(
+        dat2d_mean.reshape(data.shape[:3]), header=data_file.header, affine=data_file.header.get_best_affine()
+    )
+    nib.save(nifti_image, f'{out}_mean.nii.gz')
+
+    dat2d = dat2d - dat2d_mean
+    for atlas_idx in np.unique(atlas):
+        if atlas_idx == 0:
+            continue
+
+        dat4pca = dat2d[:, atlas.flatten() == atlas_idx]
+        U, S, Vh = np.linalg.svd(dat4pca, full_matrices=False)
+        # from sklearn.decomposition import PCA
+        # pca = PCA(n_components=dat4pca.shape[1])
+        # principal_components = pca.fit_transform(dat4pca) # 此处的dat4pca可以是未减均值的
+        # principal_components = dat4pca @ Vh.T = U * S
+        # 以上三个方法计算的PC至少在前若干个主成分应该是相同的，为简便计，采用numpy svd进行计算
+        # space * n_components
+        dat2d4save = np.zeros((dat2d.shape[1], Vh.shape[0]))
+        dat2d4save[atlas.flatten() == atlas_idx, :] = Vh.T
+        dat4save = dat2d4save.reshape((*data.shape[:3], dat2d4save.shape[1]))
+        nifti_image = nib.Nifti1Image(dat4save, header=data_file.header, affine=data_file.header.get_best_affine())
+        nib.save(nifti_image, f'{out}_Vmat_{int(atlas_idx):05}.nii.gz')
+
+        principal_components = U * S
+        # principal_components space * n_components, 每一行是一个被试，每一列是一个成分
+        np.savetxt(f'{out}_PC_{int(atlas_idx):05}.txt', principal_components)
+        explained_variance = S ** 2 / (U.shape[0] - 1)
+        explained_variance_ratio = S ** 2 / sum(S ** 2)
+        # singular value, explained_variance, explained_variance_ratio
+        pca_info = np.vstack([S, explained_variance, explained_variance_ratio]).T
+        np.savetxt(f'{out}_PCA_Info_{int(atlas_idx):05}.txt', pca_info)
+
+def setup_fpca(subparsers):
+    parser = subparsers.add_parser("fpca", help="functional PCA")
+    parser.set_defaults(func=fpca)
+    parser.add_argument('data', help='4d nifti file')
+    parser.add_argument('atlas', help='atlas file')
+    parser.add_argument('out', help='output prefix')
+
+
+@arg_extractor
 def cifti_array2map(atlas, data, out, delimiter=' ', transpose=False, skiprows=0):
     atlas = nib.load(atlas)
     data = np.loadtxt(data, delimiter=delimiter, skiprows=skiprows)
@@ -86,7 +153,7 @@ def setup_cifti_array2map(subparsers):
     parser.add_argument('-s', '--skiprows', type=int, default=0, help='skip first rows')
 
 def main():
-    parser = argparse.ArgumentParser(description="A script supporting multiple subcommands.")
+    parser = argparse.ArgumentParser(description="赩林")
     subparsers = parser.add_subparsers(dest="command", required=True, help="Subcommand to run")
 
     # Dynamically find and register all setup_* functions
